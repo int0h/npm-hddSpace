7c421eb1576d64f436e8a182fddb4e3f06871420
int0h <int0h@yandex.ru>
2017-08-04 03:23:34 +0300

    more platform tests

--- src/tests/unix.ts
+++ src/tests/unix.ts
@@ -10,7 +10,7 @@ function prepareFixture(fixture: string, prefix: string | RegExp): string {
 		.join('\n');
 }
 
-test('fixture #1', t => {
+test('fixture #1 (bananian)', t => {
 	let out = `
 		Filesystem     1K-blocks    Used Available Use% Mounted on
 		rootfs          15506408 1149728  13708188   8% /
@@ -71,7 +71,7 @@ test('fixture #1', t => {
 	t.deepEqual(result, expected);
 });
 
-test('fixture #2', t => {
+test('fixture #2 (ubuntu: russian locale)', t => {
 	let out = `
 		Файл.система   1024-блоков Использовано Доступно Вместимость Cмонтировано в
 		udev               2485944            0  2485944          0% /dev
@@ -161,6 +161,299 @@ test('fixture #2', t => {
 		total: {
 			size: 73350348800,
 			free: 20355121152,
+			place: '/',
+			mountOn: '/'
+		}
+	};
+	t.deepEqual(result, expected);
+});
+
+test('fixture #3 (macos)', t => {
+	let out = `
+		Filesystem    1024-blocks     Used Available Capacity  Mounted on
+		/dev/disk0s2    145961032 20678788 125026244    15%    /
+		devfs                 178      178         0   100%    /dev
+		map -hosts              0        0         0   100%    /net
+		map auto_home           0        0         0   100%    /home
+	`;
+	out = prepareFixture(out, /^\t\t/);
+	const result = parseUnixOutput(out);
+	const expected: HddInfo = {
+		parts: [
+			{
+				size: 149464096768,
+				free: 128026873856,
+				place: '/',
+				mountOn: '/'
+			},
+			{
+				size: 182272,
+				free: 0,
+				place: '/dev',
+				mountOn: '/dev'
+			},
+			{
+				size: 0,
+				free: 0,
+				place: '/net',
+				mountOn: '/net'
+			},
+			{
+				size: 0,
+				free: 0,
+				place: '/home',
+				mountOn: '/home'
+			}
+		],
+		total: {
+			size: 149464096768,
+			free: 128026873856,
+			place: '/',
+			mountOn: '/'
+		}
+	};
+	t.deepEqual(result, expected);
+});
+
+test('fixture #4 (ubuntu: russian locale)', t => {
+	let out = `
+		Filesystem     1024-blocks    Used Available Capacity Mounted on
+		udev                446816       0    446816       0% /dev
+		tmpfs                89884   10756     79128      12% /run
+		/dev/mmcblk0p1    15379328 2843308  12350496      19% /
+		tmpfs               449408      88    449320       1% /dev/shm
+		tmpfs                 5120       4      5116       1% /run/lock
+		tmpfs               449408       0    449408       0% /sys/fs/cgroup
+		tmpfs               449408       8    449400       1% /tmp
+		log2ram              51200    6324     44876      13% /var/log
+		tmpfs                89884       8     89876       1% /run/user/1000
+	`;
+	out = prepareFixture(out, /^\t\t/);
+	const result = parseUnixOutput(out);
+	const expected: HddInfo = {
+		parts: [
+			{
+				size: 457539584,
+				free: 457539584,
+				place: '/dev',
+				mountOn: '/dev'
+			},
+			{
+				size: 92041216,
+				free: 81027072,
+				place: '/run',
+				mountOn: '/run'
+			},
+			{
+				size: 15748431872,
+				free: 12646907904,
+				place: '/',
+				mountOn: '/'
+			},
+			{
+				size: 460193792,
+				free: 460103680,
+				place: '/dev/shm',
+				mountOn: '/dev/shm'
+			},
+			{
+				size: 5242880,
+				free: 5238784,
+				place: '/run/lock',
+				mountOn: '/run/lock'
+			},
+			{
+				size: 460193792,
+				free: 460193792,
+				place: '/sys/fs/cgroup',
+				mountOn: '/sys/fs/cgroup'
+			},
+			{
+				size: 460193792,
+				free: 460185600,
+				place: '/tmp',
+				mountOn: '/tmp'
+			},
+			{
+				size: 52428800,
+				free: 45953024,
+				place: '/var/log',
+				mountOn: '/var/log'
+			},
+			{
+				size: 92041216,
+				free: 92033024,
+				place: '/run/user/1000',
+				mountOn: '/run/user/1000'
+			}
+		],
+		total: {
+			size: 15748431872,
+			free: 12646907904,
+			place: '/',
+			mountOn: '/'
+		}
+	};
+	t.deepEqual(result, expected);
+});
+
+test('fixture #4 (ubuntu)', t => {
+	let out = `
+		Filesystem     1024-blocks       Used Available Capacity Mounted on
+		udev               1013224          0   1013224       0% /dev
+		tmpfs               204872       5036    199836       3% /run
+		/dev/sda1        139967416   20720196 112114244      16% /
+		tmpfs              1024356         80   1024276       1% /dev/shm
+		tmpfs                 5120          4      5116       1% /run/lock
+		tmpfs              1024356          0   1024356       0% /sys/fs/cgroup
+		cgmfs                  100          0       100       0% /run/cgmanager/fs
+		none            1953512444 1138175980 815336464      59% /media/sf_SharedTB
+		tmpfs               204872         48    204824       1% /run/user/1000
+	`;
+	out = prepareFixture(out, /^\t\t/);
+	const result = parseUnixOutput(out);
+	const expected: HddInfo = {
+		parts: [
+			{
+				size: 1037541376,
+				free: 1037541376,
+				place: '/dev',
+				mountOn: '/dev'
+			},
+			{
+				size: 209788928,
+				free: 204632064,
+				place: '/run',
+				mountOn: '/run'
+			},
+			{
+				size: 143326633984,
+				free: 114804985856,
+				place: '/',
+				mountOn: '/'
+			},
+			{
+				size: 1048940544,
+				free: 1048858624,
+				place: '/dev/shm',
+				mountOn: '/dev/shm'
+			},
+			{
+				size: 5242880,
+				free: 5238784,
+				place: '/run/lock',
+				mountOn: '/run/lock'
+			},
+			{
+				size: 1048940544,
+				free: 1048940544,
+				place: '/sys/fs/cgroup',
+				mountOn: '/sys/fs/cgroup'
+			},
+			{
+				size: 102400,
+				free: 102400,
+				place: '/run/cgmanager/fs',
+				mountOn: '/run/cgmanager/fs'
+			},
+			{
+				size: 2000396742656,
+				free: 834904539136,
+				place: '/media/sf_SharedTB',
+				mountOn: '/media/sf_SharedTB'
+			},
+			{
+				size: 209788928,
+				free: 209739776,
+				place: '/run/user/1000',
+				mountOn: '/run/user/1000'
+			}
+		],
+		total: {
+			size: 143326633984,
+			free: 114804985856,
+			place: '/',
+			mountOn: '/'
+		}
+	};
+	t.deepEqual(result, expected);
+});
+
+test('fixture #4 (debian: russian locale)', t => {
+	let out = `
+		Файловая система 1024-блоков Использовано Доступно Вместимость Cмонтировано в
+		/dev/sda1          110386008      4867332 99888316          5% /
+		udev                   10240            0    10240          0% /dev
+		tmpfs                 204876         4908   199968          3% /run
+		tmpfs                 512188          160   512028          1% /dev/shm
+		tmpfs                   5120            4     5116          1% /run/lock
+		tmpfs                 512188            0   512188          0% /sys/fs/cgroup
+		tmpfs                 102440            8   102432          1% /run/user/117
+		tmpfs                 102440           12   102428          1% /run/user/1000
+		/dev/sr0               58100        58100        0        100% /media/cdrom0
+	`;
+	out = prepareFixture(out, /^\t\t/);
+	const result = parseUnixOutput(out);
+	const expected: HddInfo = {
+		parts: [
+			{
+				size: 113035272192,
+				free: 102285635584,
+				place: '/',
+				mountOn: '/'
+			},
+			{
+				size: 10485760,
+				free: 10485760,
+				place: '/dev',
+				mountOn: '/dev'
+			},
+			{
+				size: 209793024,
+				free: 204767232,
+				place: '/run',
+				mountOn: '/run'
+			},
+			{
+				size: 524480512,
+				free: 524316672,
+				place: '/dev/shm',
+				mountOn: '/dev/shm'
+			},
+			{
+				size: 5242880,
+				free: 5238784,
+				place: '/run/lock',
+				mountOn: '/run/lock'
+			},
+			{
+				size: 524480512,
+				free: 524480512,
+				place: '/sys/fs/cgroup',
+				mountOn: '/sys/fs/cgroup'
+			},
+			{
+				size: 104898560,
+				free: 104890368,
+				place: '/run/user/117',
+				mountOn: '/run/user/117'
+			},
+			{
+				size: 104898560,
+				free: 104886272,
+				place: '/run/user/1000',
+				mountOn: '/run/user/1000'
+			},
+			{
+				size: 59494400,
+				free: 0,
+				place: '/media/cdrom0',
+				mountOn: '/media/cdrom0'
+			}
+		],
+		total: {
+			size: 113035272192,
+			free: 102285635584,
 			place: '/',
 			mountOn: '/'
 		}
--- src/tests/win32.ts
+++ src/tests/win32.ts
@@ -10,7 +10,7 @@ function prepareFixture(fixture: string, prefix: string | RegExp): string {
 		.join('\n');
 }
 
-test('fixture #1', t => {
+test('fixture #1 (windows 7)', t => {
 	let out = `
 		Caption  FreeSpace	Size
 		C:	   1286164480   34359734272
@@ -46,6 +46,144 @@ test('fixture #1', t => {
 		total: {
 			size: 1075487690752,
 			free: 80703885312
+		}
+	};
+	t.deepEqual(result, expected);
+});
+
+test('fixture #2 (windows xp)', t => {
+	let out = `
+		Caption  FreeSpace     Size
+		C:       627703808     10725732352
+		D:       0             699783168
+		E:       25851797504   107496861696
+		F:       834660716544  2000396742656
+	`;
+	out = prepareFixture(out, /^\t\t/);
+	const result = parseWin32Output(out);
+	const expected: HddInfo = {
+		parts: [
+			{
+				place: 'C:',
+				letter: 'C:',
+				free: 627703808,
+				size: 10725732352
+			},
+			{
+				place: 'D:',
+				letter: 'D:',
+				free: 0,
+				size: 699783168
+			},
+			{
+				place: 'E:',
+				letter: 'E:',
+				free: 25851797504,
+				size: 107496861696
+			},
+			{
+				place: 'F:',
+				letter: 'F:',
+				free: 834660716544,
+				size: 2000396742656
+			}
+		],
+		total: {
+			size: 2119319119872,
+			free: 861140217856
+		}
+	};
+	t.deepEqual(result, expected);
+});
+
+test('fixture #3 (windows server 2008)', t => {
+	let out = `
+		Caption  FreeSpace    Size
+		C:       22377099264  53580132352
+		D:       0            58601472
+	`;
+	out = prepareFixture(out, /^\t\t/);
+	const result = parseWin32Output(out);
+	const expected: HddInfo = {
+		parts: [
+			{
+				place: 'C:',
+				letter: 'C:',
+				free: 22377099264,
+				size: 53580132352
+			},
+			{
+				place: 'D:',
+				letter: 'D:',
+				free: 0,
+				size: 58601472
+			}
+		],
+		total: {
+			size: 58601472 + 53580132352,
+			free: 22377099264
+		}
+	};
+	t.deepEqual(result, expected);
+});
+
+test('fixture #4 (windows 8)', t => {
+	let out = `
+		Caption  FreeSpace     Size
+		C:       498562174976  539028877312
+		D:       0             59494400
+	`;
+	out = prepareFixture(out, /^\t\t/);
+	const result = parseWin32Output(out);
+	const expected: HddInfo = {
+		parts: [
+			{
+				place: 'C:',
+				letter: 'C:',
+				free: 498562174976,
+				size: 539028877312
+			},
+			{
+				place: 'D:',
+				letter: 'D:',
+				free: 0,
+				size: 59494400
+			}
+		],
+		total: {
+			size: 59494400 + 539028877312,
+			free: 498562174976
+		}
+	};
+	t.deepEqual(result, expected);
+});
+
+test('fixture #5 (windows 10)', t => {
+	let out = `
+		Caption  FreeSpace     Size
+		C:       159345410048  171204145152
+		D:       0             4001759232
+	`;
+	out = prepareFixture(out, /^\t\t/);
+	const result = parseWin32Output(out);
+	const expected: HddInfo = {
+		parts: [
+			{
+				place: 'C:',
+				letter: 'C:',
+				free: 159345410048,
+				size: 171204145152
+			},
+			{
+				place: 'D:',
+				letter: 'D:',
+				free: 0,
+				size: 4001759232
+			}
+		],
+		total: {
+			size: 171204145152 + 4001759232,
+			free: 159345410048
 		}
 	};
 	t.deepEqual(result, expected);
